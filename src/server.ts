fb6ca175f29d95fae07d534d3075da318eeeaa4d
Luana <luana.smatos01@gmail.com>
2024-07-13 18:31:02 -0300

    Primeiro commit

---
+++ src/server.ts
@@ -0,0 +1,33 @@
+import fastify from "fastify";
+import { createTrip } from "./routes/create-trip";
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { confirmTrip } from "./routes/confirm-trip";
+import cors from "@fastify/cors";
+import { confirmParticipant } from "./routes/confirm-participant";
+import { createActivity } from "./routes/create-activity";
+import { getActivities } from "./routes/get-activities";
+import { createLink } from "./routes/create-link";
+import { getLinks } from "./routes/get-links";
+import { getParticipants } from "./routes/get-participants";
+
+const app = fastify();
+
+app.register(cors, {
+    origin: '*',
+});
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createTrip);
+app.register(confirmTrip);
+app.register(confirmParticipant);
+app.register(createActivity);
+app.register(getActivities);
+app.register(createLink);
+app.register(getLinks);
+app.register(getParticipants);
+
+app.listen({ port: 3333 }).then(() => {
+    console.log("server running");
+})
