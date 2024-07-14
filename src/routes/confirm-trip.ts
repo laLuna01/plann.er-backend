8507cb4abdca7834de3ec57984725394fff988e8
Luana-SMatos <luana.smatos01@gmail.com>
2024-07-14 14:47:39 -0300

    Finalizando

--- src/routes/confirm-trip.ts
+++ src/routes/confirm-trip.ts
@@ -5,6 +5,8 @@ import { prisma } from "../lib/prisma";
 import { getMailClient } from "../lib/mail";
 import { dayjs } from "../lib/dayjs";
 import nodemailer from "nodemailer";
+import { ClientError } from "../errors/client-error";
+import { env } from "../env";
 
 export async function confirmTrip(app: FastifyInstance) {
     app.withTypeProvider<ZodTypeProvider>().get('/trips/:tripId/confirm', {
@@ -29,11 +31,11 @@ export async function confirmTrip(app: FastifyInstance) {
         });
 
         if (!trip) {
-            throw new Error('Trip not found.');
+            throw new ClientError('Trip not found.');
         };
 
         if (trip.is_confirmed) {
-            return reply.redirect(`http://localhost:5173/trips/${tripId}`)
+            return reply.redirect(`${env.WEB_BASE_URL}/trips/${tripId}`)
         };
 
         await prisma.trip.update({
@@ -48,7 +50,7 @@ export async function confirmTrip(app: FastifyInstance) {
 
         await Promise.all(
             trip.participants.map(async (participant) => {
-                const confirmationLink = `http://localhost:3333/participants/${participant.id}/confirm`;
+                const confirmationLink = `${env.API_BASE_URL}/participants/${participant.id}/confirm`;
 
                 const message = await mail.sendMail({
                     from: {
@@ -76,6 +78,6 @@ export async function confirmTrip(app: FastifyInstance) {
             })
         );
 
-        return reply.redirect(`http://localhost:5173/trips/${tripId}`)
+        return reply.redirect(`${env.WEB_BASE_URL}/trips/${tripId}`)
     })
 }
--- src/routes/get-trip-details.ts
+++ src/routes/get-trip-details.ts
@@ -4,8 +4,8 @@ import { z } from "zod";
 import { prisma } from "../lib/prisma";
 import { ClientError } from "../errors/client-error";
 
-export async function getParticipants(app: FastifyInstance) {
-    app.withTypeProvider<ZodTypeProvider>().get('/trips/:tripId/participants', {
+export async function getTripDetails(app: FastifyInstance) {
+    app.withTypeProvider<ZodTypeProvider>().get('/trips/:tripId', {
         schema: {
             params: z.object({
                 tripId: z.string().uuid(),
@@ -15,25 +15,22 @@ export async function getParticipants(app: FastifyInstance) {
         const { tripId } = request.params;
 
         const trip = await prisma.trip.findUnique({
+            select: {
+                id: true,
+                destination: true,
+                starts_at: true,
+                ends_at: true,
+                is_confirmed: true,
+            },
             where: {
                 id: tripId,
-            },
-            include: {
-                participants: {
-                    select: {
-                        id: true,
-                        name: true,
-                        email: true,
-                        is_confirmed: true,
-                    },
-                },
-            },
+            }
         });
 
         if (!trip) {
             throw new ClientError('Trip not found.');
         }
 
-        return { participants: trip.participants }
+        return { trip }
     })
 }
